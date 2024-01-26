5be91f6b0f90afb70b5b854f2a561965033d36eb
Alexey Filipovich <102030503+terrign@users.noreply.github.com>
2024-01-26 15:26:50 +0300

    Revert "Env setup"

--- src/components/Button/Button.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-
-import { Button } from '.';
-
-it('renders', () => {
-  const { container } = render(<Button primary={true} />);
-  expect(container).toBeInTheDocument();
-});
--- src/types/jest-dom.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="@testing-library/jest-dom/types/jest.d.ts" />
-/// <reference types="@types/jest" />
